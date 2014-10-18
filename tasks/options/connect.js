36195638f275e3661cdc8a7c28b938c98e767536
David Laing <david@davidlaing.com>
2014-10-18 10:25:36 +0100

    Add some mock /statuspage/component-status API responses to the grunt server command for use during DEV

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -4,7 +4,24 @@ module.exports = function(config) {
       options: {
         port: 5601,
         base: config.srcDir,
-        keepalive: true
+        keepalive: true,
+        middleware: function(connect) {
+            return [
+              // for the modules to test
+              connect.static(config.srcDir),
+              // Inject mock data for call to /statuspage/component-status (which in live proxies to the StatusPage.io API)
+              function(req, res, next) {
+                if (req.url !== '/statuspage/component-status') {
+                    return next();
+                }
+                res.writeHead(200, {'Content-Type': 'application/json' });
+                res.end('{"status":"operational","name":"cityindex.logsearch.io (logsearch cluster)","created_at":"2013-08-06T14:34:56.571Z","updated_at":"2014-10-18T08:52:48.210Z","position":1,"description":"The City Index Logsearch cluster","id":"2vx2724ljl64","page_id":"rr6mvdg2czkt"}');
+                //res.end('{"status":"degraded_performance","name":"cityindex.logsearch.io (logsearch cluster)","created_at":"2013-08-06T14:34:56.571Z","updated_at":"2014-10-18T09:15:59.961Z","position":1,"description":"The City Index Logsearch cluster","id":"2vx2724ljl64","page_id":"rr6mvdg2czkt"}');
+                //res.end('{"status":"partial_outage","name":"cityindex.logsearch.io (logsearch cluster)","created_at":"2013-08-06T14:34:56.571Z","updated_at":"2014-10-18T09:16:41.968Z","position":1,"description":"The City Index Logsearch cluster","id":"2vx2724ljl64","page_id":"rr6mvdg2czkt"}');
+                //red.end('{"status":"major_outage","name":"cityindex.logsearch.io (logsearch cluster)","created_at":"2013-08-06T14:34:56.571Z","updated_at":"2014-10-18T09:17:10.048Z","position":1,"description":"The City Index Logsearch cluster","id":"2vx2724ljl64","page_id":"rr6mvdg2czkt"}');
+              }
+            ];
+        }
       }
     },
     unit_tests: {
